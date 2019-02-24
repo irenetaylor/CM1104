3a29aa66fffcda484ccea176f7562c373a3aeeb5
IRENE <irenetaylor3000@gmail.com>
2019-02-24 23:17:14 +0000

    Lab 2 exercice 1

--- src/lab1_exercice2.java
+++ src/lab1_exercice2.java
@@ -2,5 +2,15 @@ public class lab1_exercice2 {
 
     public static void main(String[] args) {
 
+        int dice1;
+        int dice2;
+        for (int i = 1; i<=10;i++){
+           dice1 = (int) (Math.random()*6) + 1;
+           dice2 = (int) (Math.random()*6) + 1;
+        System.out.println("Roll " + i + " produced ");
+        System.out.println(dice1 + " and " + dice2);
+
+
+        }
     }
 }
