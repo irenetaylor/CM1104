cc3080583d50e6cb6da40edae10f8a7931ba7538
IRENE <irenetaylor3000@gmail.com>
2019-02-24 22:52:35 +0000

    Lab 2 exercice 1.0

---
+++ src/lab2_exercice1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class lab2_exercice1 {
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("The Volume of a Cone");
+            System.out.print("Please input the cone height: ");
+            double height = in.nextDouble();
+            System.out.print("Please input the cone base radius: ");
+            double radius = in.nextDouble();
+            double volume = Math.PI / 3 * radius * radius * height;
+            System.out.printf("The volume is %.3f cubic units\n", volume);
+
+        }
+    }
+
